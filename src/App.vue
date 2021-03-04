e75c4b1be32ee11ca5b4f6e964435ce5daf74d0f
bnanchen <nanchenbastian@gmail.com>
2021-03-04 14:39:52 +0100

    New Website Design

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  name: 'App',
+  mounted() {
+    console.log(document.body.style);
+  }
+})
+</script>
+
+<style lang="scss">
+$randomNumber: random(4);
+
+body {
+  height: 100%;
+  background: url(assets/backgrounds/2.jpeg) no-repeat center center fixed;
+  -webkit-background-size: cover;
+  -moz-background-size: cover;
+  -o-background-size: cover;
+  background-size: cover;
+}
+
+#app {
+  font-family: 'EB Garamond', serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="home">
+    <h1>Bastian Nanchen</h1>
+    <nav>
+      <a href="https://github.com/bnanchen" target="_blank">
+        Github
+      </a>
+      <a href="https://www.linkedin.com/in/bastian-nanchen-a3216140/" target="_blank">
+        LinkedIn
+      </a>
+      <a href="https://letterboxd.com/bnanchen/" target="_blank">
+        Letterboxd
+      </a>
+      <a href="mailto:nanchenbastian@gmail.com">
+        Contact Me
+      </a>
+    </nav>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  name: 'Home'
+});
+</script>
+
+<style lang="scss" scoped>
+h1 {
+  font-weight: 500;
+  font-size: 3rem;
+  margin: 7rem 0rem 1rem 0rem;
+}
+
+nav {
+  padding: 30px;
+
+  a {
+    font-size: 1.5rem;
+    font-weight: 400;
+    color: #2c3e50;
+    text-decoration: none;
+    margin-right: 1.25rem;
+  }
+}
+</style>
