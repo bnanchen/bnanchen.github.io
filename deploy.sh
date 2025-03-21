ecee6a67e4cc62529ff81d8969dd9ff85d75c4d0
bnanchen <nanchenbastian@gmail.com>
2025-03-21 12:04:27 +0100

    Improve deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,4 @@
 #!/usr/bin/env sh
-# abort on errors
-set -e
-# build
-yarn run build
-# navigate into the build output directory
-cd dist
-# if you are deploying to a custom domain
-# echo 'www.example.com' > CNAME
-git init
-git add -A
-git commit -m 'deploy'
-git push -f git@github.com:bnanchen/bnanchen.github.io.git master:gh-pages
-cd -#!/usr/bin/env sh
 
 # Abort on any error
 set -e
